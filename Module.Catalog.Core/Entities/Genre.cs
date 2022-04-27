212a35f3cfdfd5050a99fb01d795febf8619dc86
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 21:03:33 +0300

    Use File Scoped Namespace Directives in Entity Classes

--- Module.Catalog.Core/Entities/Genre.cs
+++ Module.Catalog.Core/Entities/Genre.cs
@@ -1,13 +1,12 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Module.Catalog.Core.Entities
-{
-    public class Genre
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
+namespace Module.Catalog.Core.Entities;
 
-        [JsonIgnore]
-        public ICollection<Movie> Movies { get; set; }
-    }
+public class Genre
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Movie> Movies { get; set; }
 }
--- Module.Catalog.Core/Entities/Movie.cs
+++ Module.Catalog.Core/Entities/Movie.cs
@@ -1,14 +1,13 @@
-﻿namespace Module.Catalog.Core.Entities
-{
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Year { get; set; }
-        public string Synopsis { get; set; }
-        public string Director { get; set; }
+﻿namespace Module.Catalog.Core.Entities;
 
-        public ICollection<Actor> Actors { get; set; }
-        public ICollection<Genre> Genres { get; set; }
-    }
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string Synopsis { get; set; }
+    public string Director { get; set; }
+
+    public ICollection<Actor> Actors { get; set; }
+    public ICollection<Genre> Genres { get; set; }
 }
