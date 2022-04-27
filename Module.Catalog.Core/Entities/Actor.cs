c2334efa9657f63e1e82a86d019eebf890436fbf
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 12:44:13 +0300

    Add Entity Classes

---
+++ Module.Catalog.Core/Entities/Actor.cs
@@ -0,0 +1,13 @@
+﻿namespace Module.Catalog.Core.Entities
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
