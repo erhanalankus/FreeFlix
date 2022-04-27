c9b24638be071d638a8d7d4eedb46faa426d9692
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 12:50:00 +0300

    Add Base DbContext Class

---
+++ Shared.Infrastructure/Persistence/ModuleDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Infrastructure.Persistence
+{
+    /// <summary>
+    /// This will be the base of all the DbContext classes in each and every module.
+    /// </summary>
+    public abstract class ModuleDbContext : DbContext
+    {
+        /// <summary>
+        /// We are using Schemas to make a logical separation between the database tables. For example, tables associated with Catalog module will be named Catalog.Movies, etc.
+        /// </summary>
+        protected abstract string Schema { get; }
+
+        protected ModuleDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            if (!string.IsNullOrWhiteSpace(Schema))
+            {
+                modelBuilder.HasDefaultSchema(Schema);
+            }
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            //// TODO Try this if the above line to apply all configurations fail:
+            //new ActorEntityTypeConfiguration().Configure(modelBuilder.Entity<Actor>());
+            //new GenreEntityTypeConfiguration().Configure(modelBuilder.Entity<Genre>());
+            //new MovieEntityTypeConfiguration().Configure(modelBuilder.Entity<Movie>());
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return (await base.SaveChangesAsync(true, cancellationToken));
+        }
+    }
+}
