b9972077bb555232df158d01f013b5b08e63fd6a
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 14:22:10 +0300

    Add GetAllMoviesQuery MediatR Handler

---
+++ Module.Catalog.Core/Queries/GetAllMoviesQuery.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Module.Catalog.Core.Abstractions;
+using Module.Catalog.Core.Entities;
+
+namespace Module.Catalog.Core.Queries
+{
+    public class GetAllMoviesQuery : IRequest<IEnumerable<Movie>>
+    {
+
+    }
+
+    internal class MovieQueryHandler : IRequestHandler<GetAllMoviesQuery, IEnumerable<Movie>>
+    {
+        private readonly ICatalogDbContext _context;
+
+        public MovieQueryHandler(ICatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Movie>> Handle(GetAllMoviesQuery request, CancellationToken cancellationToken)
+        {
+            var movies = await _context.Movies.OrderByDescending(m => m.Year).ToListAsync();
+
+            if (movies == null) throw new Exception("Movies Not Found!");
+
+            return movies;
+        }
+    }
+}
