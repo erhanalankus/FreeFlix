cd46b43065658afb5a8e6305dd4c7615e032b81c
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 12:29:33 +0300

    Register AddSharedInfrastructure to API and Comment Out .Services.AddControllers() Call

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,12 @@
+using Shared.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddSharedInfrastructure(builder.Configuration);
+
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
