404e1b2af8584d24cf2734c7ae972ceed651d0d6
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 12:32:43 +0300

    Add MoviesController

---
+++ Module.Catalog/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Module.Catalog.Controllers
+{
+    [ApiController]
+    [Route("/api/catalog/[controller]")]
+    internal class MoviesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok();
+        }
+    }
+}
