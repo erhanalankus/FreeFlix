0eae43d242c4ff02d79c6647470319d3b271d06f
Erhan Alankus <e.alankus@gmail.com>
2022-04-27 12:25:25 +0300

    Add AddSharedInfrastructure Method which will Add Controllers in Differnet Projects

---
+++ Shared.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Infrastructure.Controllers;
+
+namespace Shared.Infrastructure.Extensions
+{
+    /// <summary>
+    /// This class will be responsible for adding controllers that are in different projects.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSharedInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddControllers()
+                .ConfigureApplicationPartManager(manager =>
+                {
+                    manager.FeatureProviders.Add(new InternalControllerFeatureProvider());
+                });
+
+            return services;
+        }
+    }
+}
