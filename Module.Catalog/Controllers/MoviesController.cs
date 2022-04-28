3e3f107977c2056499287fff8e184aa07f128e17
Erhan Alankus <e.alankus@gmail.com>
2022-04-28 16:16:00 +0300

    Change Some Controller Details

--- Module.Catalog/Controllers/MoviesController.cs
+++ Module.Catalog/Controllers/MoviesController.cs
@@ -1,55 +1,79 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Module.Catalog.Core.Features.Commands;
 using Module.Catalog.Core.Features.Queries;
+using System.Net.Mime;
 
 namespace Module.Catalog.Controllers
 {
+    /*
+     * The [ApiController] attribute makes attribute routing a requirement.
+     * 
+     * The [ApiController] attribute makes model validation errors automatically trigger an HTTP 400 response.
+     * Consequently, the following code is unnecessary in an action method:
+     * if (!ModelState.IsValid)
+     * {
+     *     return BadRequest(ModelState);
+     * }
+     */
     [ApiController]
     [Route("/api/catalog/[controller]")]
     internal class MoviesController : ControllerBase
     {
         private IMediator _mediator;
 
-        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        public MoviesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
 
-        //public MoviesController(IMediator mediator)
-        //{
-        //    _mediator = mediator;
-        //}
+        //TODO Maybe inform client if there's nothing to get
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> GetAllAsync()
         {
-            var movies = await Mediator.Send(new GetAllMoviesQuery());
+            var movies = await _mediator.Send(new GetAllMoviesQuery());
 
             return Ok(movies);
         }
 
         [HttpGet("{id}", Name = "GetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Produces(MediaTypeNames.Application.Json)]
         public async Task<IActionResult> GetById(int id)
         {
-            var movie = await Mediator.Send(new GetMovieByIdQuery { Id = id });
+            var movie = await _mediator.Send(new GetMovieByIdQuery { Id = id });
 
             return Ok(movie);
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Delete(int id)
         {
-            return Ok(await Mediator.Send(new DeleteMovieByIdCommand { Id = id }));
+            return Ok(await _mediator.Send(new DeleteMovieByIdCommand { Id = id }));
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Consumes(MediaTypeNames.Application.Json)]
         public async Task<IActionResult> Create(CreateMovieCommand command)
         {
-            var createdMovie = await Mediator.Send(command);
+            var createdMovie = await _mediator.Send(command);
 
             return CreatedAtAction(nameof(MoviesController.GetById), new { id = createdMovie.Id }, createdMovie);
         }
 
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Consumes(MediaTypeNames.Application.Json)]
         public async Task<IActionResult> Update(int id, UpdateMovieCommand command)
         {
             if (id != command.Id)
@@ -57,7 +81,7 @@ namespace Module.Catalog.Controllers
                 return BadRequest();
             }
 
-            return Ok(await Mediator.Send(command));
+            return Ok(await _mediator.Send(command));
         }
     }
 }
