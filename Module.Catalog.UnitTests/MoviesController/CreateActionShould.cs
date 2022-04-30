588d53aa70474bc10ae80c9f4d1c430cc7962a65
Erhan Alankus <e.alankus@gmail.com>
2022-04-30 23:48:53 +0300

    Add Test for Create

--- Module.Catalog.UnitTests/MoviesController/CreateActionShould.cs
+++ Module.Catalog.UnitTests/MoviesController/CreateActionShould.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Module.Catalog.Core.Entities;
+using Module.Catalog.Core.Features.Commands;
+using Moq;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Xunit;
 
 namespace Module.Catalog.UnitTests.MoviesController
@@ -10,15 +12,38 @@ namespace Module.Catalog.UnitTests.MoviesController
     public class CreateActionShould
     {
         [Fact]
-        public void ReturnCreatedAtActionIfTheMovieIsCreated()
+        public async void ReturnCreatedAtActionIfTheMovieIsCreated()
         {
+            // Arrange
+            var movie = new Movie
+            {
+                Id = 1,
+                Title = "Fight Club",
+                Director = "David Fincher",
+                Synopsis = "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+                Year = "1999",
+                Actors = new List<string>
+                    {
+                        "Brad Pitt",
+                        "Edward Norton",
+                        "Helena Bonham Carter"
+                    },
+                Genres = new List<string>
+                    {
+                        "Drama"
+                    }
+            };
+            var mockMediator = new Mock<IMediator>();
+            mockMediator
+                .Setup(m => m.Send(It.IsAny<CreateMovieCommand>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(movie);
+            var moviesController = new Controllers.MoviesController(mockMediator.Object);
 
-        }
+            // Act
+            var result = await moviesController.Create(new CreateMovieCommand());
 
-        [Fact]
-        public void ReturnBadRequestIfTheInputIsInvalid()
-        {
-
+            // Assert
+            Assert.IsType<CreatedAtActionResult>(result);
         }
     }
 }
