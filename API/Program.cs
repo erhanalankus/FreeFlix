fb5af2925d222241cb0c324c2c3f0e97eec4c90a
Erhan Alankus <e.alankus@gmail.com>
2022-05-01 18:22:31 +0300

    Add OpenAPI Info

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Module.Catalog.Extensions;
 using Shared.Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,20 @@ builder.Services.AddCatalogModule(builder.Configuration);
 //builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Movies Database",
+        Description = "A Proof-of-Concept Movie Database API",
+        Contact = new OpenApiContact
+        {
+            Name = "Erhan Alankus",
+            Email = "erhanalankus@gmail.com"
+        }
+    });
+});
 
 var app = builder.Build();
 
