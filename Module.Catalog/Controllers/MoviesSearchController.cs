7b708d90775b69233e2e18f00eeda9e1bc37d765
Erhan Alankus <e.alankus@gmail.com>
2022-05-01 19:52:14 +0300

    Improve MoviesSearch Controller

--- Module.Catalog/Controllers/MoviesSearchController.cs
+++ Module.Catalog/Controllers/MoviesSearchController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Module.Catalog.Core.Entities.DTO;
 using Module.Catalog.Core.Features.Queries;
+using System.Net.Mime;
 using System.Text.Json;
 
 namespace Module.Catalog.Controllers;
@@ -17,14 +20,27 @@ public class MoviesSearchController : ControllerBase
     }
 
     [HttpGet("{movieTitleToSearchFor}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<MovieDTO>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> SearchByTitle(string movieTitleToSearchFor)
     {
         var movies = await _mediator.Send(new SearchMoviesByTitleQuery { MovieTitleToSearchFor = movieTitleToSearchFor });
+
+        if (!movies.Any())
+        {
+            return NotFound();
+        }
 
         return Ok(movies);
     }
 
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<MovieDTO>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
     public async Task<IActionResult> SearchExtended(SearchMoviesExtendedQuery query)
     {
         var movies = await _mediator.Send(query);
@@ -32,6 +48,11 @@ public class MoviesSearchController : ControllerBase
         if (Response is not null)
         {
             Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(movies.PaginationMetadata));
+        }
+
+        if (!movies.Movies.Any())
+        {
+            return NotFound();
         }
 
         return Ok(movies.Movies);
