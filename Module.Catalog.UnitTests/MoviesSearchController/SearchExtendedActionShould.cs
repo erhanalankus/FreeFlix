2dbeddbc278144aacfc297747ef539a4925cea79
Erhan Alankus <e.alankus@gmail.com>
2022-05-01 19:18:51 +0300

    Fix Extended Search Test

--- Module.Catalog.UnitTests/MoviesSearchController/SearchExtendedActionShould.cs
+++ Module.Catalog.UnitTests/MoviesSearchController/SearchExtendedActionShould.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Module.Catalog.Core.Entities.DTO;
 using Module.Catalog.Core.Features.Queries;
 using Moq;
+using System.Collections.Generic;
 using System.Threading;
 using Xunit;
 
@@ -17,11 +18,27 @@ public class SearchExtendedActionShould
         var mockMediator = new Mock<IMediator>();
         mockMediator
             .Setup(m => m.Send(It.IsAny<SearchMoviesExtendedQuery>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(It.IsAny<PaginatedMoviesDTO>);
+            .ReturnsAsync(new PaginatedMoviesDTO
+            {
+                Movies = new List<MovieDTO>(),
+                PaginationMetadata = new PaginationMetadata(10, 1, 3)
+            });
+
         var moviesSearchController = new Controllers.MoviesSearchController(mockMediator.Object);
 
+        var query = new SearchMoviesExtendedQuery
+        {
+            Page = 1,
+            ItemsPerPage = 3,
+            Year = "string",
+            Title = "string",
+            Director = "string",
+            Actors = new string[] { "string" },
+            Genres = new string[] { "drama" }
+        };
+
         // Act
-        var result = await moviesSearchController.Search("string");
+        var result = await moviesSearchController.SearchExtended(query);
 
         // Assert
         Assert.IsType<OkObjectResult>(result);
